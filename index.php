7a3d5eb98dad5e182416328777e608f30b6ba362
jimmymsz <jimmymsz@ymail.com>
2017-02-19 20:58:24 +0700

    main files

---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title> Forum abc </title>
+</head>
+<body class="abc-main-body">
+	<div id="abc">
+		<div id="header">
+			<div id="link-abc-logo">
+				<a class="abc-logo" href="index.php">
+					<img class="logo-img" src="logo.png" alt="abc" height="100" width="130">
+					<span id="abc-title-logo">Forum abc</span>
+				</a>
+			</div>
+		</div>
+		<div id="menu">
+			<div id="menu-left">
+			</div>
+			<div id="menu-right">
+			</div>
+		</div>
+	</div>
+</body>
+</html>
