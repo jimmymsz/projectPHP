1623f8b539568d24a9efdb72add0951ca8efca68
jimmymsz <jimmymsz@ymail.com>
2017-02-19 23:18:50 +0700

    css improvement and design

--- index.php
+++ index.php
@@ -10,20 +10,27 @@
 </head>
 <body class="abc-main-body">
 	<div id="abc">
-		<div id="header">
+		<div class="header">
 			<div id="link-abc-logo">
 				<a class="abc-logo" href="index.php">
 					<img class="logo-img" src="logo.png" alt="abc" height="100" width="130">
 					<span id="abc-title-logo">Forum abc</span>
 				</a>
 			</div>
-		</div>
-		<div id="menu">
-			<div id="menu-left">
+			<div id="header-menu">
+				<div class="header-menu-login">
+					<a class="btnLogin" href="login.php">Login</a>
+					<a class="btnRegister" href="register.php">Register</a>
+				</div>
 			</div>
-			<div id="menu-right">
+		</div>
+		<div class="menu">
+			<div id="menu_left">
+			</div>
+			<div id="menu_right">
 			</div>
 		</div>
+
 	</div>
 </body>
 </html>
