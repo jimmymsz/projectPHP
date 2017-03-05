e0d46aed953ce54742d97f19a748d8308a7bbb97
jimmymsz <jimmymsz@ymail.com>
2017-03-05 16:47:24 +0700

    add dropdown navigation, still bugged

--- index.php
+++ index.php
@@ -26,6 +26,32 @@
 		</div>
 		<div class="menu">
 			<div id="menu_left">
+				<ul id="navigation">
+					<li class="small70">
+						<a class="non-link" href="#">Forum</a>
+						<ul class="nav-dropdown">
+							<li>Search Forum</li>
+							<li>Recent Posts</li>
+						</ul>
+					</li>
+					<li class="small80">
+						<a class="non-link" href="#">Member</a>
+						<ul class="nav-dropdown">
+							<li>Notable Member</li>
+							<li>Forum Admin</li>
+							<li>New Members</li>
+						</ul>
+					</li>
+					<li class="small70">
+						<a class="non-link" href="#">Help (?)</a>
+						<ul class="nav-dropdown">
+							<li>About</li>
+							<li>Support</li>
+							<li>FAQs</li>
+							<li>Send Feedbacks<li>
+						</ul>
+					</li>
+				</ul>
 			</div>
 			<div id="menu_right">
 			</div>
