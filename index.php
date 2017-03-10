e2f10f7cff0a72949b22e450c93acf5ea01637a1
jimmymsz <jimmymsz@ymail.com>
2017-03-10 15:16:13 +0700

    remodeling main design

--- index.php
+++ index.php
@@ -67,6 +67,12 @@
 			</div>
 		</div>
 
+		<div class="contentWrapper">
+			<div>
+				<h1 class="h1">Welcome to Forum abc</h1>
+			</div>
+		</div>
+
 		<ul><li> <a href="">abcd</a></li><li> <a href="">1234</a></li></ul>
 
 	</div>
--- login.php
+++ login.php
@@ -7,6 +7,18 @@
 	<title>abc - Login Page</title>
 </head>
 <body class="abc-login-body">
-
+	<h1>Login</h1>
+	<div id="login_div" style="text-align: center">
+		<form method="POST" action="login.php">
+			Username
+			<input type="text" name="username">
+			<br><br>
+			Password
+			<input type="password" name="password">
+			<br>
+			<input type="submit" name="submit" value="Login">
+			<a href="register.php"> Register here </a>
+		</form>
+	</div>
 </body>
 </html>
