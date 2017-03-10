8b943cbcb66531975b32176e29d6d3dcd6174d68
jimmymsz <jimmymsz@ymail.com>
2017-03-10 15:51:36 +0700

    register page

--- register.php
+++ register.php
@@ -4,9 +4,73 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<link rel="stylesheet" type="text/css" href="style.css">
 	<title>abc - Register Page</title>
 </head>
 <body class="abc-register-body">
+	<div id="abc">
+		<div class="header">
+			<div id="link-abc-logo">
+				<a class="abc-logo" href="index.php">
+					<img class="logo-img" src="logo.png" alt="abc" height="100" width="130">
+					<span id="abc-title-logo">Forum abc</span>
+				</a>
+			</div>
+			<div id="header-menu">
+				<div class="header-menu-login">
+					<a class="btnLogin" href="login.php">Login</a>
+					<a class="btnRegister" href="register.php">Register</a>
+				</div>
+			</div>
+		</div>
+		<div class="menu">
+			<div id="menu_left">
+				<ul id="navigation">
+					<li class="small70">
+						<a class="non-link" href="#">Forum</a>
+						<ul class="nav-dropdown">
+							<li> <a href="">Search Forum</a></li>
+							<li> <a href="">Recent Posts</a></li>
+						</ul>
+					</a></li>
+					<li class="small80">
+						<a class="non-link" href="#">Member</a>
+						<ul class="nav-dropdown">
+							<li> <a href="">Notable Member</a></li>
+							<li> <a href="">Forum Admin</a></li>
+							<li> <a href="">New Members</a></li>
+						</ul>
+					</a></li>
+					<li class="small70">
+						<a class="non-link" href="#">Help (?)</a>
+						<ul class="nav-dropdown">
+							<li> <a href="">About</a></li>
+							<li> <a href="">Support</a></li>
+							<li> <a href="">FAQs</a></li>
+							<li> <a href="">Send Feedbacks</a></li>
+						</ul>
+					</a></li>
+					<li class="large">
+						<a class="non-link" href="#">Experimental</a>
+						<ul class="nav-dropdown">
+							<li>Coming Soon</li>
+							<!--
+							<li> <a href="">Coming Soon...</a></li>
+							-->
+						</ul>
+					</a></li>
+				</ul>
+			</div>
+			<div id="menu_right">
+			</div>
+		</div>
 
+		<div class="contentWrapper">
+			<div>
+				<h1 class="h1">Register to Forum abc</h1>
+			</div>
+		</div>
+
+	</div>
 </body>
 </html>
