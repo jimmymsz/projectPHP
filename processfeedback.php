da1c0b67fac4481821bdf6d07c31172f1228666a
jimmymsz <jimmymsz@ymail.com>
2017-03-12 21:23:36 +0700

    processFeedback file

---
+++ processfeedback.php
@@ -0,0 +1,23 @@
+<?php
+	//variables
+	$name = $_POST['name'];
+	$email = $_POST['email'];
+	$feedback = $_POST['feedback'];
+
+	$content = 'User name : '.$name."\n"
+				.'User e-mail : '.$email."\n"
+				."Comments : \n".$feedback."\n";
+
+	//insert to database?
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>abc - Feedback Submitted</title>
+</head>
+<body>
+	<h1>Feedback submitted</h1>
+	<p>Your feedback has been sent.</p>
+</body>
+</html>
