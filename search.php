11ce6fe85de059c17d43cc517cedc99dfd6eaf9b
Zemhart <norulanz@gmail.com>
2017-03-20 12:09:51 +0700

    fix bug at search

--- search.php
+++ search.php
@@ -91,12 +91,12 @@
 					break;
 			}
 			?>
-			<?php if (count($groups)!=0) {
+			<?php if (isset($groups) && count($groups)!=0) {
 				foreach ($groups as $g) { ?>
 				<ul>
 					<li><a href="search.php?page=grup&idGroup=<?php echo $g['idGroup'];?>"><?php echo $g['groupName'];?></li>
 				</ul>
-			<?php }} else if (count($groups) == 0) {
+			<?php }} else if (isset($groups) && count($groups) == 0) {
 				echo "Grup tidak ditemukan<br>";
 			}
 				if(isset($_COOKIE["login"])) { ?>
